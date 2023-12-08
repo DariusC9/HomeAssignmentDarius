da7820953824461825de333c05cc877bd78d1036
Couti Darius <coutidarius@gmail.com>
2023-12-08 18:30:34 +0200

    add tableview to ViewController

--- HomeAssignmentDarius/View/ViewController.swift
+++ HomeAssignmentDarius/View/ViewController.swift
@@ -10,10 +10,10 @@ import UIKit
 class ViewController: UIViewController {
     
     let subtitleLabel = UILabel()
+    let tableView = UITableView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         
         style()
         layout()
@@ -28,19 +28,31 @@ class ViewController: UIViewController {
         subtitleLabel.text = "CONTACTELE MELE"
         subtitleLabel.textColor = UIColor.labelColor
         subtitleLabel.font = UIFont.labelFont
+        /// tableView
+        tableView.backgroundColor = UIColor.white
     }
     
     private func layout() {
         /// add subviews
         view.addSubview(subtitleLabel)
+        view.addSubview(tableView)
         
         /// translatesAutoresizingMaskIntoConstraints
         subtitleLabel.translatesAutoresizingMaskIntoConstraints = false
+        tableView.translatesAutoresizingMaskIntoConstraints = false
         
         /// constraints
+        /// label
         NSLayoutConstraint.activate([
             subtitleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 12),
             subtitleLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 19)
+        ])
+        /// tableView
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: subtitleLabel.bottomAnchor, constant: 12),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -40)
         ])
     }
 }
