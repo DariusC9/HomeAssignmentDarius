ddf4afbbdd51fafa6612e87853286999291dc11a
Couti Darius <coutidarius@gmail.com>
2023-12-08 17:48:38 +0200

    create UIColor extension for background color

---
+++ HomeAssignmentDarius/Model/Extensions/UIColor extensions.swift
@@ -0,0 +1,15 @@
+//
+//  UIColor extensions.swift
+//  HomeAssignmentDarius
+//
+//  Created by Darius Couti on 08.12.2023.
+//
+
+import UIKit
+
+extension UIColor {
+    
+    static var backgroundColor: UIColor {
+        return UIColor(red: 239/255, green: 242/255, blue: 247/255, alpha: 1.0)
+    }
+}
