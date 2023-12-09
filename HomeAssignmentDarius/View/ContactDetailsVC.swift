d6e081dca8ecfe0794b3b5e30ce9f805b2b5a9f6
Couti Darius <coutidarius@gmail.com>
2023-12-09 16:14:59 +0200

    create ContactDetailsVC and ContactDetailsView

---
+++ HomeAssignmentDarius/View/ContactDetailsVC.swift
@@ -0,0 +1,92 @@
+//
+//  ContactDetailsVC.swift
+//  HomeAssignmentDarius
+//
+//  Created by Darius Couti on 09.12.2023.
+//
+
+import UIKit
+
+/// view controller used to display the details of a contact or to add a new contact
+class ContactDetailsVC: UIViewController {
+    
+    let detailsStackView = UIStackView()
+    let firstNameView = ContactDetailsView()
+    let secondNameView = ContactDetailsView()
+    let telefonView = ContactDetailsView()
+    let emailView = ContactDetailsView()
+    let button = UIButton()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        style()
+        layout()
+    }
+    
+    @objc func buttonPressed(sender: UIButton!) {
+      print("Button pressed")
+    }
+}
+
+//MARK: - Setup view extension
+
+extension ContactDetailsVC {
+    
+    private func style() {
+        /// view controller
+        self.title = "Adauga contact"
+        view.backgroundColor = UIColor.backgroundColor
+        
+        /// collectionView
+        detailsStackView.backgroundColor = UIColor.white
+        detailsStackView.distribution = .fillEqually
+        detailsStackView.axis = .vertical
+        
+        /// button
+        button.setTitle("Salveaza", for: .normal)
+        button.addTarget(self, action: #selector(buttonPressed), for: .touchUpInside)
+        button.backgroundColor = UIColor(red: 68/255, green: 200/255, blue: 77/255, alpha: 1.0)
+        button.layer.cornerRadius = 15
+        button.layer.shadowColor = UIColor.lightGray.cgColor
+        button.layer.shadowOpacity = 0.6
+        button.layer.shadowOffset = CGSize(width: 0, height: 4)
+        button.layer.shadowRadius = 4
+    }
+    
+    private func layout() {
+        /// add subviews
+        view.addSubview(detailsStackView)
+        detailsStackView.addArrangedSubview(firstNameView)
+        detailsStackView.addArrangedSubview(secondNameView)
+        detailsStackView.addArrangedSubview(telefonView)
+        detailsStackView.addArrangedSubview(emailView)
+        view.addSubview(button)
+        
+        /// translatesAutoresizingMaskIntoConstraints
+        detailsStackView.translatesAutoresizingMaskIntoConstraints = false
+        button.translatesAutoresizingMaskIntoConstraints = false
+        
+        /// constraints
+        /// detailsStackView
+        NSLayoutConstraint.activate([
+            detailsStackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            detailsStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            detailsStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        ])
+        /// stack views
+        NSLayoutConstraint.activate([
+            firstNameView.heightAnchor.constraint(equalToConstant: 110),
+            secondNameView.heightAnchor.constraint(equalToConstant: 110),
+            telefonView.heightAnchor.constraint(equalToConstant: 110),
+            emailView.heightAnchor.constraint(equalToConstant: 110)
+        ])
+        /// button
+        NSLayoutConstraint.activate([
+            button.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            button.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            button.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -40),
+            button.heightAnchor.constraint(equalToConstant: 50)
+        ])
+    }
+}
---
+++ HomeAssignmentDarius/View/ContactDetailsView.swift
@@ -0,0 +1,99 @@
+//
+//  ContactDetailsView.swift
+//  HomeAssignmentDarius
+//
+//  Created by Darius Couti on 09.12.2023.
+//
+
+import UIKit
+
+class ContactDetailsView: UIView {
+    
+    let backgroundView = UIView()
+    let titleLabel = UILabel()
+    let textField = UITextField()
+    let line = UIView()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        style()
+        layout()
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        
+        style()
+        layout()
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        backgroundView.layer.cornerRadius = 15
+        backgroundView.layer.masksToBounds = true
+    }
+
+    private func style() {
+        /// view
+        self.backgroundColor = UIColor.backgroundColor
+        
+        /// backgroundView
+        backgroundView.backgroundColor = UIColor.white
+        
+        /// titleLabel
+        titleLabel.text = "NUME"
+        titleLabel.font = UIFont.labelFont
+        titleLabel.textColor = UIColor.labelColor
+        
+        /// textField
+        textField.borderStyle = UITextField.BorderStyle.none
+        textField.text = "Nume Frumos"
+        textField.backgroundColor = UIColor.white
+        textField.textColor = UIColor.black
+        
+        /// line
+        line.backgroundColor = UIColor.backgroundColor
+    }
+    
+    private func layout() {
+        /// add subviews
+        self.addSubview(backgroundView)
+        self.addSubview(titleLabel)
+        self.addSubview(textField)
+        self.addSubview(line)
+        
+        /// translatesAutoresizingMaskIntoConstraints
+        backgroundView.translatesAutoresizingMaskIntoConstraints = false
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        line.translatesAutoresizingMaskIntoConstraints = false
+        
+        /// constraints
+        /// backgroundView
+        NSLayoutConstraint.activate([
+            backgroundView.topAnchor.constraint(equalTo: self.topAnchor, constant: 16),
+            backgroundView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 16),
+            backgroundView.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -16),
+            backgroundView.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -8)
+        ])
+        /// titleLabel
+        NSLayoutConstraint.activate([
+            titleLabel.topAnchor.constraint(equalTo: backgroundView.topAnchor, constant: 16),
+            titleLabel.leadingAnchor.constraint(equalTo: backgroundView.leadingAnchor, constant: 16),
+        ])
+        /// textField
+        NSLayoutConstraint.activate([
+            textField.topAnchor.constraint(equalTo: titleLabel.topAnchor, constant: 32),
+            textField.leadingAnchor.constraint(equalTo: backgroundView.leadingAnchor, constant: 16),
+        ])
+        /// line
+        NSLayoutConstraint.activate([
+            line.topAnchor.constraint(equalTo: textField.bottomAnchor),
+            line.leadingAnchor.constraint(equalTo: backgroundView.leadingAnchor, constant: 16),
+            line.trailingAnchor.constraint(equalTo: backgroundView.trailingAnchor, constant: -16),
+            line.heightAnchor.constraint(equalToConstant: 2)
+        ])
+    }
+}
