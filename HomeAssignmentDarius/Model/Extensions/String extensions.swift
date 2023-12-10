c8c2351976062f968fe88aa685f06fbff7c9bae6
Couti Darius <coutidarius@gmail.com>
2023-12-10 15:34:12 +0200

    Create InitialsView and String extensions

---
+++ HomeAssignmentDarius/Model/Extensions/String extensions.swift
@@ -0,0 +1,8 @@
+//
+//  String extensions.swift
+//  HomeAssignmentDarius
+//
+//  Created by Darius Couti on 10.12.2023.
+//
+
+import Foundation
---
+++ HomeAssignmentDarius/View/InitialsView.swift
@@ -0,0 +1,8 @@
+//
+//  InitialsView.swift
+//  HomeAssignmentDarius
+//
+//  Created by Darius Couti on 10.12.2023.
+//
+
+import Foundation
